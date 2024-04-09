6fa7524326532687b859023e2e9002e6bb278ea4
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-04-09 12:18:21 +0100

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { SwagTable } from "./swagTable";
+import { useEffect, useState } from "react";
+export type Swag = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  stock: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [swagData, setSwagData] = useState<Swag[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/swag.json");
+        const data = await response.json();
+        const newSwag = [];
+        newSwag.push(data);
+        setSwagData(newSwag);
+      } catch (error) {
+        console.error("Error fetching swag data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1> J.P. Morgan Payments Swag</h1>
+      <SwagTable swagData={swagData} />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/swagTable.tsx
@@ -0,0 +1,34 @@
+import { Swag } from "./App";
+
+type SwagTableProps = {
+  swagData: Swag[];
+};
+
+const addToCardClicked = (id: number) => {
+  console.log(id);
+};
+
+const SwagCard = ({ name, price, description, stock, id }: Swag) => (
+  <div className="swag-card">
+    <h2>{name}</h2>
+    <p>Price: ${price}</p>
+    <p>{description}</p>
+    <p>{stock > 0 ? "In Stock" : "Out of Stock"}</p>
+    <button disabled={stock === 0} onClick={() => addToCardClicked(id)}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export const SwagTable = ({ swagData }: SwagTableProps) => {
+  console.log(swagData);
+
+  return (
+    <div>
+      Hello from SwagTable
+      {swagData &&
+        swagData.length > 0 &&
+        swagData.map((swag) => <SwagCard key={swag.id} {...swag} />)}
+    </div>
+  );
+};
