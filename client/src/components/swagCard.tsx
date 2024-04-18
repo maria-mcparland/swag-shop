b062180791dd596f4e08572971fea559fbf84c5b
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-04-18 16:54:01 +0100

    Adding styling to button

--- client/src/components/swagCard.tsx
+++ client/src/components/swagCard.tsx
@@ -1,32 +1,48 @@
 import { Swag } from "../App";
-import React from "react";
+import React, { useState } from "react";
 type SwagCardProps = {
   item: Swag;
   onClickFunction: (id: number) => void;
 };
 
-export const SwagCard = ({ item, onClickFunction }: SwagCardProps) => (
-  <div className="flex flex-grow flex-col justify-around">
-    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-      <img
-        src={item.image}
-        alt={item.name}
-        className="h-full w-full object-cover object-center group-hover:opacity-75"
-      />
-    </div>
-    <div className="mt-4 flex justify-between mb-2">
-      <div>
-        <h3 className="text-lg text-gray-800">{item.name}</h3>
-        <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+enum ButtonText {
+  ADD_TO_BASKET = "Add to basket",
+  ADDED = "Added!",
+}
+export const SwagCard = ({ item, onClickFunction }: SwagCardProps) => {
+  const [buttonText, setButtonText] = useState(ButtonText.ADD_TO_BASKET);
+
+  const handleClick = () => {
+    setButtonText(ButtonText.ADDED);
+    onClickFunction(item.id);
+    setTimeout(() => {
+      setButtonText(ButtonText.ADD_TO_BASKET);
+    }, 1000);
+  };
+
+  return (
+    <div className="flex flex-grow flex-col justify-around">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={item.image}
+          alt={item.name}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
       </div>
-      <p className="text-sm font-medium text-gray-900">${item.price}</p>
+      <div className="mt-4 flex justify-between mb-2">
+        <div>
+          <h3 className="text-lg text-gray-800">{item.name}</h3>
+          <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">${item.price}</p>
+      </div>
+      <button
+        type="submit"
+        className={`w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+        onClick={() => handleClick()}
+      >
+        {buttonText}
+      </button>
     </div>
-    <button
-      type="submit"
-      className=" w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      onClick={() => onClickFunction(item.id)}
-    >
-      Add to basket
-    </button>
-  </div>
-);
+  );
+};
