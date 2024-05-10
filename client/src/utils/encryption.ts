e26a21ece33ecb025eabc232bad6c37914979c31
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-05-10 10:15:00 +0100

    Undo debugging and fix typo

--- client/src/utils/encryption.ts
+++ client/src/utils/encryption.ts
@@ -1,8 +1,6 @@
 import CryptoJS from "crypto-js";
 
 export const encryptData = (data: string): string => {
-  console.log(import.meta.env.VITE_KEY);
-
   const ciphertext = CryptoJS.AES.encrypt(
     data,
     import.meta.env.VITE_KEY
@@ -10,7 +8,6 @@ export const encryptData = (data: string): string => {
   return ciphertext;
 };
 export const decryptData = (data: string): string => {
-  console.log(import.meta.env.VITE_KEY);
   const bytes = CryptoJS.AES.decrypt(data, import.meta.env.VITE_KEY);
   return bytes.toString(CryptoJS.enc.Utf8);
 };
